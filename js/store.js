bc9b33775e03ce26fa9015d9c0423292ce5a4d45
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2024-10-22 20:43:56 +0800

    updated store look

--- js/store.js
+++ js/store.js
@@ -9,9 +9,9 @@ createProducts = () => {
     let productArea = document.querySelector("#product-area");
 
     let cards = `
-    <div class="col">
+    <div class="col d-flex justify-content-center">
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="https://placehold.co/300x200" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
